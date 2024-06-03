aca896381825e0c29d7d297bc476cbede7b0ac72
Otari Javakhadze <oth15@Oths-MacBook-Pro.local>
2024-06-03 18:56:12 +0400

    styled Earphones page

--- src/pages/Earphones/Earphones.tsx
+++ src/pages/Earphones/Earphones.tsx
@@ -1,13 +1,52 @@
+import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import {
+  getProducts,
+  selectProducts,
+} from '../../store/Products/Products.slice';
+import { Product } from '../../static/types';
+import './style.css';
 import Categories from '../../components/Categories';
 import Filter from '../../components/Filter';
 import Presentation from '../../components/Presentation';
-import './style.css';
+import logo from '../../assets/shared/desktop/image-category-thumbnail-earphones.png';
+
 const Earphones = () => {
+  const dispatch = useAppDispatch();
+  const products = useAppSelector(selectProducts);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  const filteredProduct = products.filter((product: Product) =>
+    [1].includes(product.id),
+  );
   return (
     <div className="headphones">
       <div className="categorie">
         <Categories categorieName="earphones" />
       </div>
+      {filteredProduct.map((product: Product) => (
+        <div key={product.id} className="product-container container">
+          <div className="img-container">
+            <img src={logo} alt={product.name} />
+          </div>
+          <div className="descr-container">
+            {product.new ? <span>new product</span> : null}
+
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <NavLink
+              to={`/product/${product.id}`}
+              className="see-product-button"
+            >
+              See Product
+            </NavLink>
+          </div>
+        </div>
+      ))}
       <Filter />
       <Presentation />
     </div>
