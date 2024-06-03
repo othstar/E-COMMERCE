882f703875547869b11f3db70378a407e2d16b79
Otari Javakhadze <oth15@Oths-MacBook-Pro.local>
2024-06-03 22:27:23 +0400

    added speakers in speakers page

--- src/pages/Speakers/Speakers.tsx
+++ src/pages/Speakers/Speakers.tsx
@@ -10,6 +10,7 @@ import Categories from '../../components/Categories';
 import Filter from '../../components/Filter';
 import Presentation from '../../components/Presentation';
 import './style.css';
+
 const Speakers = () => {
   const dispatch = useAppDispatch();
   const products = useAppSelector(selectProducts);
@@ -21,25 +22,31 @@ const Speakers = () => {
   const filteredProduct = products.filter(
     (product: Product) => product.category === 'speakers',
   );
+
   return (
     <div className="speakers">
       <div className="categorie">
         <Categories categorieName="speakers" />
       </div>
       <Filter />
+
       {filteredProduct.map((product: Product) => (
-        <div key={product.id} className="speaker-container">
-          <div className="speaker-image"></div>
-          <div className="speaker-descr">
-            {product.new ? <span>new product</span> : null}
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <NavLink
-              to={`/product/${product.id}`}
-              className="see-product-button"
-            >
-              See Product
-            </NavLink>
+        <div key={product.id} className="main container">
+          <div className="speaker-container">
+            <div className="speaker-image">
+              <img src={product.image.mobile} alt={product.name} />
+            </div>
+            <div className="speaker-descr">
+              {product.new ? <span>new product</span> : null}
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <NavLink
+                to={`/product/${product.id}`}
+                className="see-product-button"
+              >
+                see Product
+              </NavLink>
+            </div>
           </div>
         </div>
       ))}
